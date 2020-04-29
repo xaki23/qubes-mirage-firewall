0eacffb6173b959fc1ee6b966105f669f6898b28
linse <sschirme@gmail.com>
2020-04-29 15:58:01 +0200

    Read firewall rules from QubesDB. The module Rules contains a rule matcher instead of hardcoded rules now.
    Co-Authored-By: Mindy Preston <yomimono@users.noreply.github.com>

--- config.ml
+++ config.ml
@@ -14,6 +14,9 @@ let table_size =
   let key = Arg.opt ~stage:`Both Arg.int 5_000 info in
   create "nat_table_size" key
 
+let mirage_nat_pin = "git+https://github.com/roburio/mirage-nat.git#remove-connections"
+let mirage_qubes_pin = "git+https://github.com/roburio/mirage-qubes.git#qubesdb-with-commit"
+
 let main =
   foreign
     ~keys:[Functoria_key.abstract table_size]
@@ -30,13 +33,14 @@ let main =
       package "netchannel" ~min:"1.11.0";
       package "mirage-net-xen";
       package "ipaddr" ~min:"4.0.0";
-      package "mirage-qubes" ~min:"0.8.0";
-      package "mirage-nat" ~min:"2.1.0";
+      package "mirage-qubes" ~pin:mirage_qubes_pin;
+      package "mirage-nat" ~pin:mirage_nat_pin;
       package "mirage-logs";
       package "mirage-xen" ~min:"5.0.0";
+      package "pf-qubes";
     ]
-    "Unikernel.Main" (mclock @-> job)
+    "Unikernel.Main" (random @-> mclock @-> job)
 
 let () =
-  register "qubes-firewall" [main $ default_monotonic_clock]
+  register "qubes-firewall" [main $ default_random $ default_monotonic_clock]
     ~argv:no_argv
